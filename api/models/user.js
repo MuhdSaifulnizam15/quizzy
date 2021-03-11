3c5433ed342346fbb0a1dcf389916807cf6b8d4d
Muhd Saiful <muhdsaifulnizam1998@gmail.com>
2021-03-12 01:19:35 +0800

    feat: added user schema

---
+++ api/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: Number,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    image: {
+        type: String
+    }
+});
+
+userSchema.plugin(uniqueValidator, { message: 'Email already in use.' });
+module.exports = mongoose.model('User', userSchema);
