634813a7f18998d4a57e52c3983c91a7a590eac7
Muhd Saiful <muhdsaifulnizam1998@gmail.com>
2021-03-13 16:45:19 +0800

    refactor: model file name and structure

---
+++ src/api/models/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+    Address: require('./address.model'),
+    Assignment: require('./assignment.model'),
+    City: require('./city.model'),
+    Classroom: require('./classroom.model'),
+    District: require('./district.model'),
+    MotivationQuote: require('./motivation_quote.model'),
+    Question: require('./question.model'),
+    QuestionAnswer: require('./question_answer.model'),
+    QuestionOption: require('./question_option.model'),
+    QuestionType: require('./question_type.model'),
+    Quiz: require('./quiz.model'),
+    QuizMark: require('./quiz_mark.model'),
+    Subject: require('./subject.model'),
+    User: require('./user.model'),
+};
--- src/api/models/user.model.js
+++ src/api/models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+const { roles } = require('../config/roles');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
